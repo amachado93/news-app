6b7b6ff24e552dd0a06167246f1ef00f9fead501
Alex Machado <amachado@sva.edu>
2021-06-01 12:31:32 -0400

    Get onChange and onSubmit functioning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import axios from 'axios';
+//import axios from 'axios';
 
 import { fetchData } from './services/weather';
 
@@ -18,18 +18,18 @@ class App extends Component {
     }
   }
 
-  async componentDidMount(){
-    //do a call.  !! important !! do not put semi-colon after get method
-    axios.get('https://api.openweathermap.org/data/2.5/weather?zip=33154,us&units=imperial&appid=0962293081cc023b431ef06db58213c7')
-    //update state with response
-    .then( data =>{
-      console.log(data)
-      this.setState({
-        weatherData: data.data.main,
-        location: data.data.name
-      })
-    })
-  }
+  // async componentDidMount(){
+  //   //do a call.  !! important !! do not put semi-colon after get method
+  //   axios.get('https://api.openweathermap.org/data/2.5/weather?zip=33154,us&units=imperial&appid=0962293081cc023b431ef06db58213c7')
+  //   //update state with response
+  //   .then( data =>{
+  //     console.log(data)
+  //     this.setState({
+  //       weatherData: data.data.main,
+  //       location: data.data.name
+  //     })
+  //   })
+  // }
 
   handleChange = (event) => {
     this.setState({
@@ -38,22 +38,31 @@ class App extends Component {
     console.log(this.state.value)
   }
 
-  handleSubmit = async (event) => {
+  handleSubmit = (event) => {
     event.preventDefault();
-
+    console.log('submitted form')
+    // takes the user's input and passes it to API call
     fetchData(this.state.value);
     
+    // sets the state to the API call on above function
     this.setState({
-      weatherData: event
+      weatherData: fetchData(this.state.value)
     });
+    
   }
 
   render(){
   return (
     <div className="App">
       <h1>WEATHER APP</h1>
-      <Input onChange={this.handleChange} />
-      <WeatherCard weatherData={this.state.weatherData} location={this.state.location} />
+      <Input 
+        onChange={this.handleChange} 
+        onSubmit={this.handleSubmit}
+      />
+      <WeatherCard 
+        weatherData={this.state.weatherData} 
+        location={this.state.location} 
+      />
     </div>
   );
   }
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,13 +3,21 @@ import React from 'react';
 const Input = (props) => {
     return(
         <>
-            <form>
+            <form onSubmit={props.onSubmit}>
                 <h1>What's the Weather in Your Area?</h1>
-                <input onChange={props.onChange} type="text" placeholder="Enter your Zipcode"/>
-                <input type="submit" value="Submit" />
+                <input 
+                    onChange={props.onChange} 
+                    type="text" 
+                    placeholder="Enter your Zipcode"
+                />
+                <input 
+                     
+                    type="submit" 
+                    value="Submit" 
+                />
             </form>
         </>
-    )
+     )
 }
 
 export default Input;
